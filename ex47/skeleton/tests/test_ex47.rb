9bf6ed65cf466d792156361ef4604be1416c0af7
phil7690 <phillipskat@gmail.com>
2018-06-13 14:16:45 -0400

    Ruby the hard way Wednesday

--- ex47/skeleton/tests/test_ex47.rb
+++ ex47/skeleton/tests/test_ex47.rb
@@ -21,7 +21,16 @@ class TestNAME < Test::Unit::TestCase
 
   def test_map()
     start = Room.new("Start", "You can go west and down a hole.")
-    west = Room.new("Trees", "There are trees here, you can ")
+    west = Room.new("Trees", "There are trees here, you can go east.")
+    down = Room.new("Dungeon", "It's dark down here, you can go up.")
 
+    start.add_paths({'west'=> west, 'down'=> down})
+    west.add_paths({'east'=> start})
+    down.add_paths({'up'=> start})
+
+    assert_equal(west, start.go('west'))
+    assert_equal(start.start.go('west').go('east'))
+    assert_equal(start, start.go('down').go('up'))
+  end
 
 end
